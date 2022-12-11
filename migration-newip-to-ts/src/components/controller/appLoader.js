8deced061284a30e08eca3ce302bf2c9f0473a80
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2022-12-11 13:33:40 +0200

    feat: add api key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '80be14f09c6247c69fb8d2fc4897b727', // получите свой ключ https://newsapi.org/
         });
     }
 }
