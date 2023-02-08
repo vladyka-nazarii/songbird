4baca95d004988d1412e213d0240edd08c97a23f
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2023-02-08 19:19:18 +0200

    refactor: sorting-listener if

--- async-race/src/listeners/sorting-listener.ts
+++ async-race/src/listeners/sorting-listener.ts
@@ -6,13 +6,12 @@ export const addSortingListener = () => {
   const winnersContainer = document.querySelector('.winners-container') as HTMLDivElement;
   winnersContainer.addEventListener('click', async (event) => {
     const target = event.target as HTMLTableCellElement;
-    if (target.id === OrderButton.Wins) {
-      store.sortBy = Sort.ByWins;
-      store.sortOrder = store.sortOrder || store.sortOrder === Order.ByDesc ? Order.ByAsc : Order.ByDesc;
-      await updateWinners();
-    }
-    if (target.id === OrderButton.Time) {
-      store.sortBy = Sort.ByTime;
+    if (target.id === OrderButton.Wins || target.id === OrderButton.Time) {
+      if (target.id === OrderButton.Wins) {
+        store.sortBy = Sort.ByWins;
+      } else {
+        store.sortBy = Sort.ByTime;
+      }
       store.sortOrder = store.sortOrder || store.sortOrder === Order.ByDesc ? Order.ByAsc : Order.ByDesc;
       await updateWinners();
     }
